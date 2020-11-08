dae6cf1805243a6cee1d0074bdb441d09ebc085a
keshiamukenge <keshia_mkg@live.fr>
2020-11-08 11:54:58 +0100

    Avancement de la mise en page avec Bootstrap

---
+++ script.js
@@ -0,0 +1,17 @@
+//déplacer le cercle du cta "commencer" au survol avec anime.js
+
+//let animation_cta = anime({
+  //  targets: "#cercle-icon-cta-commencer",
+  //  translateX: 250,
+  //  direction: "alterner"
+ // });
+
+//Initialisation de slick.js
+ // $(document).ready(function(){
+   // $('.perso').slick({
+    //  setting-name: setting-value
+   // });
+  //});
+
+  //Choix des personnages sous forme de diaporama avec slick.js
+  //$('.single-item').slick();
