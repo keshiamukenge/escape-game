89bca6ac99242ac0ad83e4a5f12aab0176e29ae0
keshiamukenge <keshia_mkg@live.fr>
2020-11-09 18:56:29 +0100

    modification de la mise en page

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
-//déplacer le cercle du cta "commencer" au survol avec anime.js
-
-//let animation_cta = anime({
-  //  targets: "#cercle-icon-cta-commencer",
-  //  translateX: 250,
-  //  direction: "alterner"
- // });
-
-//Initialisation de slick.js
- // $(document).ready(function(){
-   // $('.perso').slick({
-    //  setting-name: setting-value
-   // });
-  //});
-
-  //Choix des personnages sous forme de diaporama avec slick.js
-  //$('.single-item').slick();
+  //Création de la fonction qui animera le bouton au clique
+  var cercle = document.getElementById("cercle-cta-next");
+  console.log(cercle);
+ 
+ 
