54c3ecd27a7997d8f5cac5a9467c4608fac3f15d
keshiamukenge <keshia_mkg@live.fr>
2020-11-10 13:45:58 +0100

    Modification de la mise en page (page-défi1)

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-  //Création de la fonction qui animera le bouton au clique
-  var cercle = document.getElementById("cercle-cta-next");
-  console.log(cercle);
- 
- 
+//Création de l'intéractivité du pad munérique
