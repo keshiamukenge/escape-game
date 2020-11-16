c55c91093d996f1bad60fb52416617b7734a0b45
keshiamukenge <keshia_mkg@live.fr>
2020-11-16 12:49:52 +0100

    Ajout d'indices + redactions des règles du jeu

--- script.js
+++ script.js
@@ -12,12 +12,12 @@ numPad.forEach(numPad => {
 	return;
 });
 
-//Correction du mot de passe
-var boutonCorrection = document.getElementById("bouton");
+//Validation du mot de passe
+var boutonValider = document.getElementById("bouton");
 var mdpDefi1 = 1389;
 var ctaNext = document.getElementById("cta-next-defi");
 
-boutonCorrection.addEventListener('click', function correctMdp (){
+boutonValider.addEventListener('click', function correctMdp(){
 	if(numMdp.value == mdpDefi1){
 		alert("Le mot de passe est correct");
 		ctaNext.style.display = "block";
@@ -26,6 +26,32 @@ boutonCorrection.addEventListener('click', function correctMdp (){
 		alert("Le mot de passe est incorrect");
 	}
 });
+
+//Affichage des indices selon un temps donné
+var indice1 = document.querySelector("#indice1");
+var indice2 = document.querySelector("#indice2");
+var indice3 = document.querySelector("#indice3");
+
+setTimeout(function() {
+	indice1.style.display = "block";
+	}, 20000);
+setTimeout(function() {
+	indice1.style.display = "none";
+}, 25000);
+
+setTimeout(function() {
+	indice2.style.display = "block";
+}, 50000);
+setTimeout(function() {
+	indice2.style.display = "none";
+}, 55000);
+
+setTimeout(function() {
+	indice3.style.display = "block";
+}, 80000);
+setTimeout(function() {
+	indice3.style.display = "none";
+}, 85000);
 
 
 
