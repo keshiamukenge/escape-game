b0f014f7fd6f38a4ac609ff57bc4131f7a7e7db5
keshiamukenge <keshia_mkg@live.fr>
2020-12-24 12:21:44 +0100

    Modification du design du site (couleurs, typo, icons)

--- script.js
+++ script.js
@@ -27,36 +27,53 @@ boutonValider.addEventListener('click', function correctMdp(){
 	}
 });
 
-//Affichage des indices selon un temps donné
-var indice1 = document.querySelector("#indice1");
-var indice2 = document.querySelector("#indice2");
-var indice3 = document.querySelector("#indice3");
+//Affichage des indices selon un temps donnée
+var indices = [document.querySelector("#indice1"), document.querySelector("#indice2"), document.querySelector("#indice3")];
 
 setTimeout(function() {
-	indice1.style.display = "block";
+	indices[0].style.display = "block";
 	}, 20000);
 setTimeout(function() {
-	indice1.style.display = "none";
+	indices[0].style.display = "none";
 }, 25000);
 
 setTimeout(function() {
-	indice2.style.display = "block";
+	indices[1].style.display = "block";
 }, 50000);
 setTimeout(function() {
-	indice2.style.display = "none";
+	indices[1].style.display = "none";
 }, 55000);
 
 setTimeout(function() {
-	indice3.style.display = "block";
+	indices[2].style.display = "block";
 }, 80000);
 setTimeout(function() {
-	indice3.style.display = "none";
+	indices[2].style.display = "none";
 }, 85000);
 
 
+//Réactivité du formulaire : Afficher "entrez un pseudo ou entrez un message pour pouvoir envoyer"
+var pseudo = document.getElementById("pseudo").value;
+var messageUtilisateur = document.getElementById("message").value;
+var boutonEnvoyer = document.getElementById("bouton-envoyer");
+
+boutonEnvoyer.addEventListener("click", function formulaire(){
+	if (pseudo.length & messageUtilisateur.length == 0){
+		alert("Vous devez entrer un pseudo et votre message");
+	} if (pseudo.length == 0) {
+		alert("Vous devez entrer un pseudo");
+	} if(messageUtilisateur.length == 0) {
+		alert("Vous devez entrer votre message");
+	} else {
+		alert("Votre message a bien été envoyer");
+	}
+});
 
 
+//Affichage page A propos au clique
+var lienApropos = document.getElementById("lien-apropos");
+var contenuApropos = document.getElementById("contenu-apropos");
 
-
-
-
+lienApropos.addEventListener("click", function (){
+	contenuApropos.style.display = "block";
+})
