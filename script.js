152325cc13c80a1c2f5fe91fdfed5251254be1f2
keshiamukenge <keshia_mkg@live.fr>
2021-01-01 17:58:38 +0100

    redesign des illustrations + modifications du mdp

--- script.js
+++ script.js
@@ -9,12 +9,11 @@ numPad.forEach(numPad => {
 	numPad.addEventListener("click", function actionMdp() {
 		numMdp.value = numMdp.value + numPad.innerHTML;
 	})
-	return;
 });
 
 //Validation du mot de passe
 var boutonValider = document.getElementById("valider");
-var mdpDefi1 = 1389;
+var mdpDefi1 = 399;
 var ctaNext = document.getElementById("cta-next-defi");
 
 boutonValider.addEventListener('click', function correctMdp(){
@@ -52,28 +51,11 @@ setTimeout(function() {
 }, 85000);
 
 
-//Réactivité du formulaire : Afficher "entrez un pseudo ou entrez un message pour pouvoir envoyer"
-var pseudo = document.getElementById("pseudo").value;
-var messageUtilisateur = document.getElementById("message").value;
-var boutonEnvoyer = document.getElementById("bouton-envoyer");
-
-boutonEnvoyer.addEventListener("click", function formulaire(){
-	if (pseudo.length & messageUtilisateur.length == 0){
-		alert("Vous devez entrer un pseudo et votre message");
-	} if (pseudo.length == 0) {
-		alert("Vous devez entrer un pseudo");
-	} if(messageUtilisateur.length == 0) {
-		alert("Vous devez entrer votre message");
-	} else {
-		alert("Votre message a bien été envoyer");
-	}
-});
-
 
 //Affichage page A propos au clique
-var lienApropos = document.getElementById("lien-apropos");
+var lienApropos = document.getElementById("a-propos");
 var contenuApropos = document.getElementById("contenu-apropos");
 
 lienApropos.addEventListener("click", function (){
-	contenuApropos.style.display = "block";
+	contenuApropos.style.visibility = "visible";
 })
